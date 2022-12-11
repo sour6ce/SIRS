2ffe87e69b9c04e0de0b1d973e456599e1ea951e
dbyta <dbyf.rguez@gmail.com>
2022-12-11 11:14:19 -0500

    feat: adding K-Means terms correlation

--- engine/generalized_vector/__gvquerifier.py
+++ engine/generalized_vector/__gvquerifier.py
@@ -1,3 +1,4 @@
+from sklearn.cluster import KMeans
 from engine.vector.__vquerifier import VectorIRQuerifier
 from ..tokenizer import tokenize
 from typing import Dict, Iterable, List, Tuple
@@ -5,8 +6,10 @@ from ..core import IRCollection, IRS
 from ..core import DOCID, INDEX
 from os import path
 import numpy as np
+from scipy.stats import pearsonr
 import pandas as pd
 
+'''
 #Correlación de Pearson
 def get_term_correlations(df: pd.DataFrame) -> Dict[str, Dict[str, float]]:
     correlations = {}
@@ -21,26 +24,60 @@ def get_term_correlations(df: pd.DataFrame) -> Dict[str, Dict[str, float]]:
             correlations[term1][term2] = corr
 
     return correlations
+'''
+#Correlacion de K-Means
+def get_term_correlations(df: pd.DataFrame, n_clusters: int = 10) -> Dict[str, Dict[str, float]]:
+    # Calculate term correlations using K-means clustering
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(df)
 
-"""
+    # Create dictionary to store term correlations
+    term_correlations = {}
+
+    # Iterate over all terms in dataframe
+    for term in df.index:
+        # Get cluster index for term
+        cluster_index = kmeans.predict([df.loc[term]])[0]
+
+        # Get cluster center for term
+        cluster_center = kmeans.cluster_centers_[cluster_index]
+
+        # Get other terms in same cluster
+        cluster_terms = df[kmeans.labels_ == cluster_index].index
+
+        # Calculate correlations for term
+        correlations = {}
+        for ct in cluster_terms:
+            # Calculate correlation coefficient between term and ct
+            coef = pearsonr(df.loc[term], df.loc[ct])[0]
+            
+            # Store correlation coefficient in dictionary
+            correlations[ct] = coef
+
+        # Add term correlations to dictionary
+        term_correlations[term] = correlations
+
+    return term_correlations
+
+
 class GeneralizedVectorIRQuerifier(VectorIRQuerifier):
     __last = None
     
     def querify(self, query: str) -> pd.DataFrame:
         # Create dictionary of term frequencies in query
-        r = {}
+        term_frequencies = {}
         for s in tokenize(query):
-            r[s] = r.get(s, 0)+1
+            term_frequencies[s] = term_frequencies.get(s, 0)+1
 
         # Convert dictionary to pandas series
-        qs = pd.Series(data=r.values(), index=r.keys())
+        qs = pd.Series(data=term_frequencies.values(), index=term_frequencies.keys())
         qdf = pd.DataFrame({'query': qs})
 
-        # Get term frequencies in documents
-        df = collection.get_term_frequencies()
-
-        # Calculate term correlations
-        term_correlations = get_term_correlations(df)
+        # Set the average of correlation, it could be change, the values are between 0 and 1
+        correlation_threshold = 0.8
+        
+        # Calculate term correlationsss
+        term_correlations = get_term_correlations(qs)
 
         # Include correlated terms in query
         for term in qdf.index:
@@ -63,9 +100,6 @@ class GeneralizedVectorIRQuerifier(VectorIRQuerifier):
         self.__last = qdf
 
         return qdf
-"""
 
-class GeneralizedVectorIRQuerifier(VectorIRQuerifier):
-    pass
 
 
