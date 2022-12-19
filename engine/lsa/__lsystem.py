c522f1fecb1077aa1243a5e1e3499bf6f5574fdc
Erick A. Fonseca Perez <erickfons00@gmail.com>
2022-12-19 13:55:27 -0500

    feat: renaming collection

--- engine/lsa/__lsystem.py
+++ engine/lsa/__lsystem.py
@@ -1,5 +1,5 @@
 from ..core import IRS
-from .__lcollection import VectorIRCollection
+from .__lcollection import LsiIRCollection
 from .__lquerifier import LsiIRQuerifier
 
 
@@ -7,4 +7,4 @@ class LatentSemanticIRS(IRS):
     def __init__(self) -> None:
         super().__init__()
         self.querifier = LsiIRQuerifier()
-        self.collection = VectorIRCollection()
+        self.collection = LsiIRCollection()
