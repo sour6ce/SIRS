6434aad48c8b82abd5eddefa8384fd7a6dd4dc24
Erick A. Fonseca Perez <erickfons00@gmail.com>
2022-12-19 14:21:19 -0500

    fix: setting querifier and collection up

--- engine/lsa/__init__.py
+++ engine/lsa/__init__.py
@@ -1,5 +1,4 @@
 from .__lcollection import *
 from .__lquerifier import *
 from .__lsystem import *
-from .__lcache import *
 from .__lindex import *
--- engine/lsa/__lquerifier.py
+++ engine/lsa/__lquerifier.py
@@ -7,27 +7,21 @@ import hashlib
 class LsiIRQuerifier(IRQuerifier):
     __last = None
 
-    def querify(self, query: str) -> pd.DataFrame:
-        # TODO: Replace DataFrame use for dict
+    def querify(self, query: str) -> Dict[str, int]:
         r = {}
         # Creates a dictionary indexed by terms that stores the
         # frequency of the term in the query (used as weight temporarily)
         for s in tokenize(query):
             r[s] = r.get(s, 0)+1
 
-        qs = pd.Series(data=r.values(), index=r.keys())
-        qdf = pd.DataFrame({'query': qs})
-        qdf.sort_index(inplace=True)
-        qdf.index.name = 'term'
+        self.__last = r
 
-        self.__last = qdf
-
-        return qdf
+        return r
 
     def get_hash(self) -> str:
         if self.__last is None:
             return ''
         else:
             h = hashlib.sha512()
-            h.update(pd.util.hash_pandas_object(self.__last).values)
+            h.update(frozenset(self.__last))
             return h.hexdigest()
