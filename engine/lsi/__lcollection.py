154f04c2cef32715c747a4982671f7539870b413
Erick A. Fonseca Perez <erickfons00@gmail.com>
2022-12-20 12:53:32 -0500

    feat: changing weighting sistem

--- engine/lsi/__lcollection.py
+++ engine/lsi/__lcollection.py
@@ -27,25 +27,7 @@ class LsiIRCollection(IRCollection):
         for doc in documents:
             self.index.add_document(doc, self.__get_index(doc))
         self.index.update_cache()
-
-    def get_tf(self, term: str, doc: DOCID):
-        '''
-        Return the tf value of a term in a document.
-        '''
-        return self.index.whole[doc, term] / self.index.max_freq[doc]
-
-    def get_ni(self, term: str):
-        '''
-        Return the number of documents where the term is. For non-indexed terms return 0.
-        '''
-        return self.index.n_i.get(term, 0)
-
-    def get_idf(self, term: str):
-        '''
-        Return the idf value of a term. For non-indexed terms return 0
-        '''
-        return self.index.idf.get(term, 0)
-
+    
     def get_documents(self) -> List[DOCID]:
         return list(self.index.docs)
     
@@ -54,27 +36,25 @@ class LsiIRCollection(IRCollection):
         pass
         
     def get_relevances(self,query: Dict[str,int]) -> List[Tuple[DOCID, float]]:
-        k=50
+        k=100
         dbt = self.index.doc_by_term.keys()
         docs = self.index.docs
-        Amat = self.index.Amat
         u = self.index.u
         s = self.index.s
+        v = self.index.v
         uk = u[:,:k]
+        vk = v[:,:k]
         skinv = np.linalg.inv(np.diag(s[:k]))
         A = 0.5
-        max_q = max(query.values())
         dbt = self.index.doc_by_term.keys()
         docs = self.index.docs
-        qt =[[(A+(1-A)*((query[term])/max_q if term in query.keys() else 0))*self.get_idf(term) for term in dbt]]
+        qt =[[((query[term]) if term in query.keys() else 0) for term in dbt]]
         ukskinv = np.matmul(uk,skinv)
         qk = np.matmul(qt,ukskinv)
-        AmatT = np.transpose(Amat)
-        Ak = [np.matmul([r],ukskinv)[0] for r in AmatT]
         res = {}
         it=0
         for doc in docs:
-            res[doc] = self.sim(qk[0],Ak[it])
+            res[doc] = self.sim(qk[0],vk[it])
             it = it+1
             
         sorted_keys = sorted(res, key=lambda x: res[x], reverse=True)
